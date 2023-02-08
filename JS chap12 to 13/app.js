a0f7b82bfd1441f5431684f3e46bf3972023f0c1
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-02-09 00:01:25 +0500

    Add files via upload

---
+++ JS chap12 to 13/app.js
@@ -0,0 +1,95 @@
+//Question#1
+
+// var character = prompt("");
+
+// if (character == character.toUpperCase()) {
+//  alert ('upper case ');
+// }
+// if (character == character.toLowerCase()){
+//  alert ('lower case ');
+// }
+
+
+//Question#2
+// var num1=+prompt("enter 1st num")
+// var num2=+prompt("enter 2nd num")
+
+// if(num1>num2){
+//     display=num1
+// }
+// else if(num2>num1){
+//     display=num2
+// }
+// else if(num1===num2){
+//     display="both are equal"
+// }
+
+// alert(display)
+
+// //Question#3
+// var num1=prompt("enter 1st num")
+// // var num2=+prompt("enter 2nd num")
+
+// if(num1>0){
+//     display="positive number"
+// }
+// else if(num1==0){
+//     display="zero"
+// }    
+// else{
+//     display="negative number"
+// }
+
+//     alert(display)
+
+//Question#4
+// var string=prompt("Enter a character")
+
+// if(string=="a" || string=="e" || string=="i" || string=="o" || string=="u"){
+//     alert("it is a vowel")
+// }
+// else(
+//     alert("it is not a vowel")
+// )
+
+//Question#5
+// paswd="qwer1234"
+// var password=prompt("enter password")
+// if(password==0){
+//     get=("please enter password")
+// }
+// else if(password==paswd){
+//     get="“Correct! The password you entered matches the original password”."
+// }
+// else{
+//     get="“Incorrect password”"
+// }
+// alert(get)
+
+//Question#6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+// alert(greeting)
+
+//Question#7
+// var time=+prompt("enter time1")
+
+// if(time>=00 && time<12){
+//     get="Good morning"
+// }
+// if(time>=12 && time<17){
+//     get="Good Afternoon"
+// }
+// if(time>=17 && time<21){
+//     get="Good evening"
+// }
+// if(time>=21 && time<24){
+//     get="Good night"
+// }
+
+// alert(get)
